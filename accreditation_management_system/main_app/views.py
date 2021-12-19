f0ece0aaf5417a23b728e93b731312a2da6d6b1c
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2021-12-19 23:15:22 +0800

    added firebase

--- accreditation_management_system/main_app/views.py
+++ accreditation_management_system/main_app/views.py
@@ -1,4 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+import requests
+import json
+
+import pyrebase
+
+import firebase_admin
+from firebase_admin import auth
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# import html to pdf converter
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
+from io import BytesIO
+from django.core.files import File
+from django.conf import settings
+
+config = {
+  'apiKey': "AIzaSyAh_oOakDenknpcWt5oucsLODSDiheWxps",
+  'authDomain': "accreditation-management.firebaseapp.com",
+  'projectId': "accreditation-management",
+  'databaseURL': "https://accreditation-management-default-rtdb.firebaseio.com/",
+  'storageBucket': "accreditation-management.appspot.com",
+  'messagingSenderId': "553200895776",
+  'appId': "1:553200895776:web:66ec853d3450869e4b3945"
+}
+
+firebase = pyrebase.initialize_app(config)
+cred = credentials.Certificate("main_app/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+auth_pyrebase = firebase.auth()
+
+firestoreDB = firestore.client()
+
+storage = firebase.storage()
 
 # Create your views here.
 def login(request):
