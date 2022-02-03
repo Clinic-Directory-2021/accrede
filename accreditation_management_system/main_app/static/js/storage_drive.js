8046cc38f507345e9bb0a473c3362250e573f6c0
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2022-02-03 18:57:38 +0800

    update

--- accreditation_management_system/main_app/static/js/storage_drive.js
+++ accreditation_management_system/main_app/static/js/storage_drive.js
@@ -166,6 +166,68 @@ else if(levelValue == 'Level 1' && areaValue == 'Area 2' && parameterValue == 'P
 '</div>';
 }
 
+else if(levelValue == 'Level 1' && areaValue == 'Area 2' && parameterValue == 'Parameter B' && categoryValue == 'Implementation'){
+  var html = 
+  '<div class="appendedField">'+
+  '<select data-role="select" name="uploadIn" id="uploadIn" form="drive_form" required>'+
+  '<option value="I.1.1">I.1.1. Academic Qualification</option>' +
+  '<option value="I.1.2">I.1.2. Personal Qualities</option>' +
+  '<option value="I.1.3">I.1.3. Communications Skills</option>' +
+  '<option value="I.1.4">I.1.4. Teaching Ability</option>' +
+  '<option value="I.1.5">I.1.5. Medical Examination Results</option>' +
+  '<option value="I.1.6">I.1.6. psychological examination result</option>' +
+  '<option value="I.1.7">I.1.7. relevant/ professional experiences</option>' +
+  '<option value="I.1.8">I.1.8. previous record of employment</option>' +
+  '<option value="I.1.9">I.1.9. Technical/ special skills & abilities</option>' +
+  '<option value="I.1.10">I.1.10. previous performance rating</option>' +
+  '<option value="I.2.1">I.2.1. Print media (newspaper, flyers, CSC Bulletin of Vacant Positions)</option>' +
+  '<option value="I.2.2">I.2.2. Broadcast  media (TV, Radio)</option>' +
+  '<option value="I.2.3">I.2.3. Websites</option>' +
+  '<option value="I.2.4">I.2.4. Bulletin Boards</option>' +
+  '<option value="I.2.5">I.2.5. Others </option>' +
+  '<option value="I.3">I.3. A Screening  Committee selects  and recommends  the best and most qualified  applicants.</option>' +
+  '<option value="I.4">I.4. Measures to avoid professional in-breeding are observed</option>' +
+  '<option value="I.5">I.5. The institution conducts orientation for newly-hire faculty  on its institutional  vision and mission; CSC, PRC, DBM, and administration  policies on their duties,  responsibilities,  benefits and other  academic concerns</option>' +
+  '</select>'+
+'</div>';
+}
+
+else if(levelValue == 'Level 1' && areaValue == 'Area 2' && parameterValue == 'Parameter B' && categoryValue == 'Outcomes'){
+  var html = 
+  '<div class="appendedField">'+
+  '<select data-role="select" name="uploadIn" id="uploadIn" form="drive_form" required>'+
+  '<option value="O.1">O.1. The most  qualified faculty are selected</option>' +
+  '</select>'+
+'</div>';
+}
+
+else if(levelValue == 'Level 1' && areaValue == 'Area 2' && parameterValue == 'Parameter B' && categoryValue == 'System'){
+  var html = 
+  '<div class="appendedField">'+
+  '<select data-role="select" name="uploadIn" id="uploadIn" form="drive_form" required>'+
+  '<option value="S.1">S.1. There is an institutional Human Resource Development Plan/ Program designed for faculty recruitment.</option>' +
+'</div>';
+}
+
+// AREA 2 PARAMETER C 
+else if(levelValue == 'Level 1' && areaValue == 'Area 2' && parameterValue == 'Parameter C' && categoryValue == 'Implementation'){
+  var html = 
+  '<div class="appendedField">'+
+  '<select data-role="select" name="uploadIn" id="uploadIn" form="drive_form" required>'+
+  '<option value="I.1">I.1. The maximum total load is  assigned to  regular full-time faculty in  accordance to the CMO of the  program and  institutional  guidelines</option>' +
+  '<option value="I.2">I.2. A Faculty  Manual is  published  formalizing  faculty policies,  standards and  guidelines</option>' +
+  '<option value="I.3">I.3. Professional  subjects are  handled by specialists in the discipline/ program</option>' +
+  '<option value="I.4">I.4. The faculty  are assigned to  teach their major/  minor fields of  specialization, for a maximum of  four (4) different subject preparations within a semester</option>' +
+  '<option value="I.5">I.5. Administrative arrangements are adopted when vacancies/leaves of  absence occur  during the term</option>' +
+  '<option value="I.6">I.6. No less than 60% of the  professional  subjects offered  in the program  are handled by  full-time faculty.</option>' +
+  '<option value="I.7">I.7. Teaching schedule does not allow more than six (6) hours of continuous teaching</option>' +
+  '<option value="I.8">I.8. Administrative, research and/or  professional assignment outside of regular teaching are given credits</option>' +
+  '<option value="I.9">I.9. Consultation,  tutorial, remedial  classes, expert  services and other instruction -  related activities  are given credits in consonance with  faculty workload guidelines</option>' +
+  '<option value="I.10">I.10. Workload assignments  and number of  preparations follow existing workload guidelines</option>' +
+  '</select>'+
+'</div>';
+}
+
 
   $(".appendedField").remove();
   $( ".appendHere" ).append( html );
